48162453e033b7b7a8d6c5f7edde1e801959610c
Glizela Taino <glizelataino@yahoo.com>
2017-03-01 19:19:56 -0600

    Can get JSON. Working on populating collection view

--- MovieSearchApp/FirstViewController.swift
+++ MovieSearchApp/FirstViewController.swift
@@ -15,6 +15,8 @@ class FirstViewController: UIViewController, UICollectionViewDataSource {
     override func viewDidLoad() {
         super.viewDidLoad()
         collectionView.dataSource = self
+        let json = getJSON(path: "http://www.omdbapi.com/?s=fast+and+furious&y=&plot=short&r=json")
+        print("\(json)")
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
@@ -31,6 +33,16 @@ class FirstViewController: UIViewController, UICollectionViewDataSource {
     
     func numberOfSections(in collectionView: UICollectionView) -> Int {
         return 1
+    }
+    
+    private func getJSON(path: String) -> JSON{
+        guard let url = URL(string: path)else { return JSON.null }
+        do {
+            let data = try Data(contentsOf: url)
+            return JSON(data: data)
+        } catch {
+            return JSON.null
+        }
     }
 
     override func didReceiveMemoryWarning() {
---
+++ MovieSearchApp/MovieCollectionViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MovieCollectionViewCell.swift
+//  MovieSearchApp
+//
+//  Created by Glizela Taino on 3/1/17.
+//  Copyright © 2017 Glizela Taino. All rights reserved.
+//
+
+import UIKit
+
+class MovieCollectionViewCell: UICollectionViewCell {
+    @IBOutlet private weak var posterImg: UIImageView!
+    @IBOutlet private weak var movieTitle: UILabel!
+    
+    func setPosterImage(image: UIImage) {
+        posterImg.image = image
+    }
+    
+    func setTitle(title: String) {
+            movieTitle.text = title
+    }
+    
+}
