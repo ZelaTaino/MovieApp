6ff399bf921aca0210398bea358f38eac4264434
Glizela Taino <glizelataino@yahoo.com>
2017-03-06 16:44:22 -0600

    Pull requests asyncs and spinner is functional

--- MovieSearchApp/FirstViewController.swift
+++ MovieSearchApp/FirstViewController.swift
@@ -12,6 +12,7 @@ class FirstViewController: UIViewController, UICollectionViewDelegate, UICollect
     
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var collectionView: UICollectionView!
+    var spinner = UIActivityIndicatorView()
     var movieArray = [Movie]()
     var cellSelected = 0
 
@@ -21,30 +22,38 @@ class FirstViewController: UIViewController, UICollectionViewDelegate, UICollect
         collectionView.delegate = self
         searchBar.delegate = self
         
+        let frame = CGRect(x: 0, y: 0, width: 37, height: 37)
+        spinner = UIActivityIndicatorView(frame: frame)
+        spinner.center.x = view.center.x
+        spinner.center.y = view.frame.size.height / 4.0
+        spinner.hidesWhenStopped = true
+        spinner.activityIndicatorViewStyle = .gray
+        collectionView.addSubview(spinner)
     }
     
     func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
-        searchBar.showsCancelButton = true
+        searchBar.setShowsCancelButton(true, animated: true)
     }
     
     func searchBarTextDidEndEditing(_ searchBar: UISearchBar) {
-        searchBar.showsCancelButton = false
+        searchBar.setShowsCancelButton(false, animated: true)
     }
     
     func searchBarCancelButtonClicked(_ aSearchBar: UISearchBar) {
         aSearchBar.text = ""
-        searchBar(aSearchBar, textDidChange: "")
-        aSearchBar.endEditing(true)
+        searchBar.endEditing(true)
     }
     
-    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        
+        spinner.startAnimating()
         movieArray.removeAll()
-        let newSearchText = searchText.replacingOccurrences(of: " ", with: "+")
+        let newSearchText = searchBar.text?.replacingOccurrences(of: " ", with: "+")
         DispatchQueue.global().async {
-            let json = DataHandler.getJSON(path: "http://www.omdbapi.com/?s=\(newSearchText)")
+            let json = DataHandler.getJSON(path: "http://www.omdbapi.com/?s=\(newSearchText!)")
             var secondJson:JSON = []
             if json["totalResults"].intValue > 10 {
-                secondJson = DataHandler.getJSON(path: "http://www.omdbapi.com/?s=\(newSearchText)&page=2")
+                secondJson = DataHandler.getJSON(path: "http://www.omdbapi.com/?s=\(newSearchText!)&page=2")
             }
             DispatchQueue.main.async {
                 self.createMovies(json: json)
@@ -52,6 +61,10 @@ class FirstViewController: UIViewController, UICollectionViewDelegate, UICollect
                     self.createMovies(json: secondJson)
                 }
                 self.collectionView.reloadData()
+                if json["Response"] != "False"{
+                    searchBar.endEditing(true)
+                }
+                self.spinner.stopAnimating()
             }
         }
     }
