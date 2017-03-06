4dae05059f80e0faa6621412fd654a2e300e5160
Glizela Taino <glizelataino@yahoo.com>
2017-03-06 12:28:27 -0600

    Set number of columns and at least 20 movies load. Work on layout of cells

--- MovieSearchApp/FirstViewController.swift
+++ MovieSearchApp/FirstViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class FirstViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UISearchBarDelegate {
+class FirstViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UISearchBarDelegate, UICollectionViewDelegateFlowLayout {
     
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var collectionView: UICollectionView!
@@ -42,8 +42,15 @@ class FirstViewController: UIViewController, UICollectionViewDelegate, UICollect
         let newSearchText = searchText.replacingOccurrences(of: " ", with: "+")
         DispatchQueue.global().async {
             let json = DataHandler.getJSON(path: "http://www.omdbapi.com/?s=\(newSearchText)")
+            var secondJson:JSON = []
+            if json["totalResults"].intValue > 10 {
+                secondJson = DataHandler.getJSON(path: "http://www.omdbapi.com/?s=\(newSearchText)&page=2")
+            }
             DispatchQueue.main.async {
                 self.createMovies(json: json)
+                if json["totalResults"].intValue > 10 {
+                    self.createMovies(json: secondJson)
+                }
                 self.collectionView.reloadData()
             }
         }
@@ -64,6 +71,25 @@ class FirstViewController: UIViewController, UICollectionViewDelegate, UICollect
     
     func numberOfSections(in collectionView: UICollectionView) -> Int {
         return 1
+    }
+    
+    let sectionInset = UIEdgeInsets(top: 10.0, left: 10.0, bottom: 10.0, right: 10.0)
+    let itemsPerRow: CGFloat = 3
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let paddingSpace = sectionInset.left * (itemsPerRow + 1)
+        let availableWidth = view.frame.width - paddingSpace
+        let widthPerItem = availableWidth / itemsPerRow
+        let heightPerItem = 41.0/(27.0/widthPerItem) // maintaining poster ratio based on width
+        return CGSize(width: widthPerItem, height: heightPerItem)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return sectionInset
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return sectionInset.left
     }
     
     private func createMovies(json: JSON) {
